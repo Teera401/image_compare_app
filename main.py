3b3183d9e07f35769def4cd0f612ddddba059036
= <freedom2523@gmail.com>
2026-02-24 14:21:48 +0700

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from image_viewer import ImageCompareViewer
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    viewer = ImageCompareViewer("images_a", "images_b")
+    viewer.show()
+
+    sys.exit(app.exec())
---
+++ zoomable_view.py
@@ -0,0 +1,34 @@
+from PySide6.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
+from PySide6.QtGui import QPainter, QPixmap
+from PySide6.QtCore import Qt
+
+
+class ZoomableImageView(QGraphicsView):
+    def __init__(self):
+        super().__init__()
+
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+
+        self.pixmap_item = QGraphicsPixmapItem()
+        self.scene.addItem(self.pixmap_item)
+
+        # self.setRenderHints(self.renderHints() | Qt.SmoothTransformation)
+        self.setRenderHints(self.renderHints() | QPainter.RenderHint.SmoothPixmapTransform)
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
+
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        self.zoom_factor = 1.25
+
+    def set_image(self, image_path):
+        pixmap = QPixmap(image_path)
+        self.pixmap_item.setPixmap(pixmap)
+        self.scene.setSceneRect(pixmap.rect())
+        self.resetTransform()
+
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            self.scale(self.zoom_factor, self.zoom_factor)
+        else:
+            self.scale(1 / self.zoom_factor, 1 / self.zoom_factor)
