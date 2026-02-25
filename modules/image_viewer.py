c25c245776f2fd7d879933f9279a28f0e88905da
= <freedom2523@gmail.com>
2026-02-25 19:47:16 +0700

    Update

--- modules/image_viewer.py
+++ modules/image_viewer.py
@@ -15,7 +15,7 @@ class ImageCompareViewer(QWidget):
         settingCnf = SettingCnfProvider()
         self.evidence_alias_mapping_dict = settingCnf.read_evidence_mapping_config()
         # read_evidence_mapping_config = settingCnf.read_evidence_mapping_config()
-        self.index = 0
+        self.index = None
         self.init_ui()
         # self.next_image()
 
@@ -23,6 +23,7 @@ class ImageCompareViewer(QWidget):
         self.setWindowTitle("Image Compare (Zoom & Pan)")
         self.resize(1200, 600)
 
+        self.label_index = QLabel()
         # Zoomable views
         self.view_a = ZoomableImageView()
         self.view_b = ZoomableImageView()
@@ -35,20 +36,28 @@ class ImageCompareViewer(QWidget):
         self.next_btn.clicked.connect(self.next_image)
 
         # Layouts Label
+        label_layout = QHBoxLayout()
         label_ref = QLabel("Reference")
         label_evidence = QLabel("Evidence")
-
-        label_layout = QHBoxLayout()
         label_layout.addWidget(label_ref)
         label_layout.addWidget(label_evidence)
 
         # Layouts
+        txt_path_layout = QHBoxLayout()
         self.tbx_path_ref = QLineEdit()
         self.tbx_path_evidence = QLineEdit()
+        txt_path_layout.addWidget(QLabel("Path Ref:"))
+        txt_path_layout.addWidget(self.tbx_path_ref)
+        txt_path_layout.addWidget(QLabel("Path Evidence:"))
+        txt_path_layout.addWidget(self.tbx_path_evidence)
 
-        txt_layout = QHBoxLayout()
-        txt_layout.addWidget(self.tbx_path_ref)
-        txt_layout.addWidget(self.tbx_path_evidence)
+        text_align_layout = QHBoxLayout()
+        self.tbx_alias_ref = QLineEdit()
+        self.tbx_alias_evidence = QLineEdit()
+        text_align_layout.addWidget(QLabel("Alias Ref:"))
+        text_align_layout.addWidget(self.tbx_alias_ref)
+        text_align_layout.addWidget(QLabel("Alias Evidence:"))
+        text_align_layout.addWidget(self.tbx_alias_evidence)
 
 
         # Layouts
@@ -58,13 +67,15 @@ class ImageCompareViewer(QWidget):
 
         btn_layout = QHBoxLayout()
         btn_layout.addStretch()
+        btn_layout.addWidget(self.label_index)
         btn_layout.addWidget(self.back_btn)
         btn_layout.addWidget(self.next_btn)
         btn_layout.addStretch()
 
         main_layout = QVBoxLayout(self)
         main_layout.addLayout(label_layout)
-        main_layout.addLayout(txt_layout)
+        main_layout.addLayout(text_align_layout)
+        main_layout.addLayout(txt_path_layout)
         main_layout.addLayout(img_layout)
         main_layout.addLayout(btn_layout)
 
@@ -74,26 +85,38 @@ class ImageCompareViewer(QWidget):
         self.view_a.set_image(ref_pic_path)
         self.view_b.set_image(evidence_pic_path)
 
-    def next_image(self):
+    def next_image(self):        
+        if self.index is None:
+            self.index = 0
+        elif self.index < len(self.evidence_alias_mapping_dict) - 1:
+                self.index += 1
         var:PicPropEvidence = list(self.evidence_alias_mapping_dict.values())[self.index] if self.index < len(self.evidence_alias_mapping_dict) else None
         if not var:
             return
+        self.label_index.setText(f"Index: {self.index} / {len(self.evidence_alias_mapping_dict)-1}")
         ref_pic_path = var.PicPropRef.full_path if var.PicPropRef else None
         evidence_pic_path = var.full_path if var else None
         self.tbx_path_ref.setText(ref_pic_path if ref_pic_path else "")
         self.tbx_path_evidence.setText(evidence_pic_path if evidence_pic_path else "")
+        self.tbx_alias_ref.setText(var.PicPropRef.alias if var.PicPropRef else "")
+        self.tbx_alias_evidence.setText(var.alias if var else "")
         self.load_images(ref_pic_path, evidence_pic_path)
-        if self.index < len(self.evidence_alias_mapping_dict) - 1:
-            self.index += 1
+
 
     def prev_image(self):
+        if self.index is None:
+            self.index = 0
+        elif self.index > 0:
+                self.index -= 1
         var:PicPropEvidence = list(self.evidence_alias_mapping_dict.values())[self.index] if self.index < len(self.evidence_alias_mapping_dict) else None
         if not var:
             return
+        self.label_index.setText(f"Index: {self.index} / {len(self.evidence_alias_mapping_dict)-1}")
         ref_pic_path = var.PicPropRef.full_path if var.PicPropRef else None
         evidence_pic_path = var.full_path if var else None
         self.tbx_path_ref.setText(ref_pic_path if ref_pic_path else "")
         self.tbx_path_evidence.setText(evidence_pic_path if evidence_pic_path else "")
+        self.tbx_alias_ref.setText(var.PicPropRef.alias if var.PicPropRef else "")
+        self.tbx_alias_evidence.setText(var.alias if var else "")
         self.load_images(ref_pic_path, evidence_pic_path)
-        if self.index > 0:
-            self.index -= 1
+
